bb3fd6bc1387227798140321d04862119f83b643
GGOShinoAsada <mstr.calling@gmail.com>
2020-11-19 18:14:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        public static int port = 8080;
+        
+        public  static void Main(string[] args)
+        {
+            SERVER.Server server = new SERVER.Server(port);
+            Socket remote = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint point = new IPEndPoint(IPAddress.Loopback, port);
+            remote.Connect(point);
+            using (NetworkStream stream = new NetworkStream(remote))
+            using (StreamReader reader = new StreamReader(stream))
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                Task recievetask = _Receive(reader);
+                string text;
+                Console.WriteLine("CLIENT: connected.Enter text to send...");
+                while ((text = Console.ReadLine()) != "")
+                {
+                    writer.WriteLine(text);
+                    writer.Flush();
+                }
+
+                remote.Shutdown(SocketShutdown.Send);
+                recievetask.Wait();
+            }
+            server.Stop();
+        }
+        private static async Task _Receive(StreamReader reader)
+        {
+            string receiveText;
+
+            while ((receiveText = await reader.ReadLineAsync()) != null)
+            {
+                Console.WriteLine("CLIENT: received \"" + receiveText + "\"");
+            }
+
+            Console.WriteLine("CLIENT: end-of-stream");
+        }
+    }
+}
---
+++ SERVER/Server.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server.SERVER
+{
+    class Server
+    {
+        public readonly Socket listen;
+        public Server(int port)
+        {
+            IPEndPoint point = new IPEndPoint(IPAddress.Loopback, port);
+            listen = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            listen.Bind(point);
+            listen.Listen(1);
+            listen.BeginAccept(_Accept, null);
+        }
+        private async void _Accept(IAsyncResult result)
+        {
+            try
+            {
+                using (Socket client = listen.EndAccept(result))
+                using (NetworkStream stream = new NetworkStream(client))
+                using (StreamReader reader = new StreamReader(stream))
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    Console.WriteLine("SERVER: accepted new clien"); 
+                    string text;
+                    while ((text = await reader.ReadLineAsync()) != null)
+                    {
+                        Console.WriteLine($"SERVER received: {text}");
+                        writer.WriteLine(text);
+                        writer.Flush();
+                    }
+                }
+                listen.BeginAccept(_Accept, null);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                Console.WriteLine("ERROR: server was closed: " + ex.Message);
+            }
+            catch (SocketException ex2)
+            {
+                Console.WriteLine("ERROR: other socket error" + ex2.Message);
+            }
+        }
+        public void Stop()
+        {
+            listen.Close();
+        }
+    }
+}
